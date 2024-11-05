4d9f449b770b3437e34a4cbc0336a6115368d330
Jciro17 <cirojeronimo17@gmail.com>
2024-11-04 23:47:18 -0500

    Se mejoro las interfaces del menu de navegacion y del login para mejora de experiencia de usuario

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,18 +6,19 @@ import { AuthService } from '@auth0/auth0-angular';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
+  user: any = null;
 
   constructor(public auth: AuthService) {}
 
   ngOnInit(): void {
-    
+    // Suscribirse a auth.user$ para obtener la información del usuario
+    this.auth.user$.subscribe(user => {
+      this.user = user;
+    });
   }
 
-  logOut(){
-
-    this.auth.logout()
-
+  logOut(): void {
+    this.auth.logout();
   }
-
 }
